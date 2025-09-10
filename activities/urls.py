f4522f8353efa8fa49fcb093470f66f281a4fb89
Amadou KOUYATE <amkou002@edu.cegepgarneau.ca>
2025-09-09 21:26:24 -0400

    feat:mise en place de la barre de navigation

--- activities/urls.py
+++ activities/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),
-]
+    path('', views.index, name='index'),
+    path('', views.proposer_activite, name='proposer_activite'),
+    path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
+    path("logout/", views.logout_view, name="logout"),
+    path("proposer/", views.proposer_activite, name="proposer_activite"),
+    path("profil/<int:pk>/", views.profil, name="profil"),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- activities/views.py
+++ activities/views.py
@@ -1,8 +1,22 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'activities/index.html')
+    return render(request, "activities/index.html")
 
+def proposer_activite():
+    return HttpResponse("Hello, world")
 
+def login_view():
+    return HttpResponse("Hello, world")
+
+def signup_view():
+    return HttpResponse("Hello, world")
+
+def logout_view():
+    return HttpResponse("Hello, world")
+
+def profil():
+    return HttpResponse("Hello, world")
