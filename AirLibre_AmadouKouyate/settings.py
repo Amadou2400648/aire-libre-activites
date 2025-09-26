cbb1da344e62d2c7eb11a0f719eb36f089e7e7ab
Amadou KOUYATE <amkou002@edu.cegepgarneau.ca>
2025-09-26 18:42:40 -0400

    feat:Dernier commit

--- AirLibre_AmadouKouyate/settings.py
+++ AirLibre_AmadouKouyate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-x*+md39j4b^8zf2h^z&dl+%l0dig1!&_a2i(f^61=*zc23u1qp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','.localhost']
 
 
 # Application definition
@@ -135,4 +135,4 @@ FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5MB
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
 
 #Clé du token pour API
-AQICN_TOKEN = "233817a21811b13d4cd9a4d15eaece97973ed527"
+AQICN_TOKEN = "9d1e9818b7c1ba5dcb6cee360c90a6a7343a93f2"
--- AirLibre_AmadouKouyate/urls.py
+++ AirLibre_AmadouKouyate/urls.py
@@ -12,8 +12,9 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('activities/', include('activities.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
